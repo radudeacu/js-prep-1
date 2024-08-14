ae94af182b240dc59de075f2b35578fc7cea6f38
radudeacu <deacuradu@gmail.com>
2024-08-14 19:25:29 +0300

    null & undefined  | hoisting

---
+++ script2.js
@@ -0,0 +1,14 @@
+// What will be console.logged here
+
+// Question 1 error
+// console.log(foo);
+// foo = 1;
+
+// Question 2 undefined
+// console.log(foo);
+// var foo = 2;
+
+// question 3      3
+foo = 3;
+console.log(foo);
+var foo;
