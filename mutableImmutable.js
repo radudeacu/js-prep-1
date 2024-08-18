ecf2752af508837a7ed1fc03dc83e1a4e53ef11b
radudeacu <deacuradu@gmail.com>
2024-08-18 19:18:34 +0300

    mutable vs immutable

---
+++ mutableImmutable.js
@@ -0,0 +1,17 @@
+// MUTABLE
+
+let arr = [1, 2, 3];
+console.log(arr);
+
+arr.push(4);
+console.log(arr);
+
+
+// IMMUTABLE
+
+let str = 'hello';
+console.log(str);
+
+str[0] = 'H';
+console.log(str);
+console.log(str[0]);
